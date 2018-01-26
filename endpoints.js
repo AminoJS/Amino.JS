3441387d33ea012dd0555e0574d2393479af13fd
Robin <moelrobi@gmail.com>
2018-01-26 14:39:38 +0100

    getChat begun. Not Completed.

--- endpoints.js
+++ endpoints.js
@@ -1,5 +1,6 @@
 let prefix = 'http://service.narvii.com/api/';
 
 module.exports = {
-	login: prefix + 'v1/g/s/auth/login'
+	login: prefix + 'v1/g/s/auth/login',
+	loadChat:(com, uuid, count) => {return `${prefix}/v1/${com}/s/chat/thread/${uuid}/message?start=0&size=${count}&cv=v1.2`}
 }
--- index.js
+++ index.js
@@ -8,14 +8,16 @@
 const request = require('request-promise');
 const endpoints = require('./endpoints.js');
 
+console.log(endpoints.loadChat("comid", "dfd", 3))
+
 module.exports = {
-	/**
-	 * Loginfunction for the Framework for Handeling API Reqeusts.
-	 * @param  {String} email    Email-Adresse für den Login
-	 * @param  {String} password Passwort für den Login
-	 * @param  {UUID} deviceID Siehe mehr unter ('howto/deviceid_dump.md')
-	 * @return {SecurityString} sid      Der Security String um mit der API zu komunizieren.
-	 */
+    /**
+     * Loginfunction for the Framework for Handeling API Reqeusts.
+     * @param  {String} email    Email-Adresse für den Login
+     * @param  {String} password Passwort für den Login
+     * @param  {UUID} deviceID Siehe mehr unter ('howto/deviceid_dump.md')
+     * @return {SecurityString} sid      Der Security String um mit der API zu komunizieren.
+     */
     login: async function(email, password, deviceID) {
         let sid;
         await request.post(endpoints.login, {
@@ -28,14 +30,27 @@ module.exports = {
                 "timestamp": new Date().getUTCMilliseconds()
             }
         }, async function(err, res, body) {
-                if (err) throw 'Request Error: ' + err;
-                if (!body.sid) throw 'Login Error: SID is not defined.' + res;
-                sid = body.sid;
-            }
-        ).catch((err) => {
+            if (err) throw 'Request Error: ' + err;
+            if (!body.sid) throw 'Login Error: SID is not defined.' + res;
+            sid = body.sid;
+        }).catch((err) => {
             throw 'Error while calling Login: ' + err;
         });
         return sid;
+    },
+
+    getChat: async function(sid, com, uid, count) {
+        let thread;
+        await request.get(endpoints.loadChat(com, uid, count), {
+            headers: {
+                'NDCAUTH': `sid=${sid}`
+            }
+        }, function(err, res, body) {
+        	body = JSON.parse(body);
+            if (err) throw 'Request Error: ' + err;
+            	console.log(body);
+            thread = body;
+        })
     }
 }
 
