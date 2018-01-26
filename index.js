883c1b7a7f21330203321a7b3ba8d8b05b49c91b
Robin <moelrobi@gmail.com>
2018-01-26 10:20:00 +0100

    Finished Login Function.

--- index.js
+++ index.js
@@ -9,20 +9,36 @@ const request = require('request-promise');
 const endpoints = require('./endpoints.js');
 
 module.exports = {
-	login: async function(email, password, deviceID) {
-		let sid = await request.post(endpoints.login, {
-			json: {
-				'email': email,
-				'password': '0 ' + password,
-				'deviceID': deviceID,
-				'clientType': 100,
-				'action': 'normal',
-				'timestamp': new Date().getUTCMilliseconds()
-			}, function(err, res, body) {
-				if(err) throw 'Framework Error: ' + err;
-				return body.sid;
-			}
-		});
-		return sid;
-	}
+	/**
+	 * Loginfunction for the Framework for Handeling API Reqeusts.
+	 * @param  {String} email    Email-Adresse für den Login
+	 * @param  {String} password Passwort für den Login
+	 * @param  {UUID} deviceID Siehe mehr unter ('howto/deviceid_dump.md')
+	 * @return {SecurityString} sid      Der Security String um mit der API zu komunizieren.
+	 */
+    login: async function(email, password, deviceID) {
+        let sid;
+        await request.post(endpoints.login, {
+            json: {
+                "email": email,
+                "secret": "0 " + password,
+                "deviceID": deviceID,
+                "clientType": 100,
+                "action": "normal",
+                "timestamp": new Date().getUTCMilliseconds()
+            }
+        }, async function(err, res, body) {
+                if (err) throw 'Request Error: ' + err;
+                if (!body.sid) throw 'Login Error: SID is not defined.' + res;
+                sid = body.sid;
+            }
+        ).catch((err) => {
+            throw 'Error while calling Login: ' + err;
+        });
+        return sid;
+    }
 }
+
+process.on('unhandledRejection', function(err) {
+    throw 'A Undhandeld unhandledRejection was Found: ' + err;
+})
--- tests/index.js
+++ tests/index.js
@@ -1,2 +1,10 @@
 let should = require('chai').should(),
 	index = require('../index.js');
+	testingconfig = require('./config/')
+	login = index.login;
+
+describe('#login', function () {
+		it('checking login..', function() {
+			login(testconfig.amino.email, testconfig.amino.password, testconfig.amino.deviceID)
+		})
+})
