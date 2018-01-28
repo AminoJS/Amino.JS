46ff81bef8d0778f6b88057cd87f152d58d92fe7
Robin <moelrobi@gmail.com>
2018-01-28 18:23:47 +0100

    Two New Modules in development

--- endpoints.js
+++ endpoints.js
@@ -2,5 +2,7 @@ let prefix = 'http://service.narvii.com/api/';
 
 module.exports = {
 	login: prefix + 'v1/g/s/auth/login',
-	loadChat:(com, uuid, count) => {return `${prefix}/v1/${com}/s/chat/thread/${uuid}/message?start=0&size=${count}&cv=v1.2`}
+	getComs: prefix + 'v1/g/s/community/joined?start=0&size=50',
+	getJoinedChats:(com) => `${prefix}/v1/${com}/s/chat/thread?type=joined-me&start=0&size=100`,
+	loadChat:(com, uuid, count) => `${prefix}/v1/${com}/s/chat/thread/${uuid}/message?start=0&size=${count}&cv=v1.2`
 }
--- index.js
+++ index.js
@@ -8,8 +8,6 @@
 const request = require('request-promise');
 const endpoints = require('./endpoints.js');
 
-console.log(endpoints.loadChat("comid", "dfd", 3))
-
 module.exports = {
     /**
      * Loginfunction for the Framework for Handeling API Reqeusts.
@@ -29,7 +27,7 @@ module.exports = {
                 "action": "normal",
                 "timestamp": new Date().getUTCMilliseconds()
             }
-        }, async function(err, res, body) {
+        }, (err, res, body) => {
             if (err) throw 'Request Error: ' + err;
             if (!body.sid) throw 'Login Error: SID is not defined.' + res;
             sid = body.sid;
@@ -39,21 +37,120 @@ module.exports = {
         return sid;
     },
 
-    getChat: async function(sid, com, uid, count) {
-        let thread;
-        await request.get(endpoints.loadChat(com, uid, count), {
-            headers: {
-                'NDCAUTH': `sid=${sid}`
-            }
-        }, function(err, res, body) {
-        	body = JSON.parse(body);
-            if (err) throw 'Request Error: ' + err;
-            	console.log(body);
-            thread = body;
+    getJoinedComs: async function(sid) {
+        let communityList = {
+            coms: [],
+            status: "not ok",
+            error: "not everything defined"
+        };
+        await request.get(endpoints.getComs, {
+                headers: {
+                    'NDCAUTH': `sid=${sid}`
+                }
+            }, (err, res, body) => {
+                try {
+                    if (err) throw 'Request Error: ' + err;
+                    body = JSON.parse(body);
+                    body.communityList.forEach((element) => {
+                        communityList.coms.push({
+                            'id': 'x' + element.ndcId,
+                            'name': element.name,
+                            'link': element.link
+                        })
+                    })
+                    communityList.status = "ok";
+                    communityList.error = null;
+                } catch (err) {
+                    communityList.status = "not ok";
+                    communityList.error = err;
+                }
         })
-    }
-}
+        return communityList;
+    },
+    
 
-process.on('unhandledRejection', function(err) {
-    throw 'A Undhandeld unhandledRejection was Found: ' + err;
-})
+getJoinedChats: async function(sid, com) {
+    let threadList = {
+        threads: [],
+        status: "not ok",
+        error: "not everything defined"
+    };
+    await request.get(endpoints.getJoinedChats(com), {
+                headers: {
+                    'NDCAUTH': `sid=${sid}`
+                }
+            }, (err, res, body) => {
+                try {
+                    /**
+                     * @todo Make a Handle for Images.
+                     */
+                    body = JSON.parse(body);
+                    body.threadList.forEach((element) => {
+                        if (element.type == 2) {
+                            public = true;
+                        } else {
+                            public = false;
+                        }
+                        if (element.type == 1 || element.type == 2) {
+                            group = true;
+                        } else {
+                            group = false;
+                        }
+                        if (element.membershipStatus == 1) {
+                            joined = true;
+                        } else {
+                            joined = false;
+                        }
+                        if (element.alertOption == 1) {
+                            muted = true;
+                        } else {
+                            muted = false;
+                        }
+                        if(element.condition == 1) {
+                            unread = true;
+                        }
+                        else {
+                            unread = false;
+                        }
+                        console.log(element.type);
+                        threadList.threads.push({
+                            'threadId': element.threadId,
+                            'memberCount': element.membersCount,
+                            'title': element.title,
+                            'joined': joined,
+                            'public': public,
+                            'group': group,
+                            'muted': muted,
+                            'unread': unread,
+                            'lastMessage': {
+                                'senderId': element.lastMessageSummary.uid,
+                                'message': element.lastMessageSummary.content
+                            },
+                            'members': element.membersSummary
+                        });
+                    })
+                    threadList.status = "ok";
+                    threadList.error = null;
+                    console.log(threadList);
+                }
+            catch (err) {
+                threadList.status = "not ok";
+                threadList.error = err;
+            }
+        })},
+
+        getChat: async function(sid, com, uid, count) {
+            let thread;
+            await request.get(endpoints.loadChat(com, uid, count), {
+                headers: {
+                    'NDCAUTH': `sid=${sid}`
+                }
+            }, (err, res, body) => {
+                body = JSON.parse(body);
+                if (err) throw 'Request Error: ' + err;
+                console.log(body);
+                thread = body;
+            });
+            return thread;
+        }
+}
