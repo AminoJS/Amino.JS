f0ef6417e6432bc14d977e09bfeef947214ddd77
Robin Möller <moelrobi@gmail.com>
2018-04-21 18:54:32 +0200

    commenting is done

--- modules/commentWikiEntry.js
+++ modules/commentWikiEntry.js
@@ -1,30 +1,68 @@
 //Libary import
 const request = require('request-promise'); //The Request Module for sending the different Modules
 const endpoints = require('../helpers/endpoints.js'); //For Creating shorter URL's in this Module
+const upload = require('../helpers/upload');
 
 const {
     getConfig
 } = require('../index');
 
-module.exports = async function createWikiEntry(com, uid, content) {
+module.exports = async function commentWikiEntry(com, uid, content, img_path) {
     let sid = getConfig('sid');
     let completed = false;
 
-    request.post(endpoints.commentWiki(com, uid), {
-        headers: {
-            NDCAUTH: `sid=${sid}`
-        },
-        json: {
-            'content': content,
-            'mediaList': [],
-            'eventSource': 'PostDetailView',
-            'timestamp': new Date().getUTCMilliseconds
-        }
-    }, (err, res, body) => {
-        if(err) throw new Error('Oops. this should not have happend', err);
+    if(img_path == undefined) {
+        request.post(endpoints.commentWiki(com, uid), {
+            headers: {
+                NDCAUTH: `sid=${sid}`
+            },
+            json: {
+                'content': content,
+                'mediaList': [],
+                'eventSource': 'PostDetailView',
+                'timestamp': new Date().getUTCMilliseconds
+            }
+        }, (err, res, body) => {
+            if(err) throw new Error('Oops. this should not have happend', err);
+    
+            if(body.comment == undefined) throw new Error('Something failed while Creating your Post');
+            else completed = true;
+        });
+    }
 
-        if(body.comment == undefined) throw new Error('Something failed while Creating your Post');
-        else completed = true;
-    });
+    else {
+        let url = await upload(img_path);
+        request.post(endpoints.commentWiki(com, uid), {
+            headers: {
+                NDCAUTH: `sid=${sid}`
+            },
+            json: {
+                'content': content,
+                'mediaList': [
+                    [
+                        100,
+                        url.value,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        'amino.jsrocks.png',
+                        0
+                    ]
+                ],
+                'eventSource': 'PostDetailView',
+                'timestamp': new Date().getUTCMilliseconds
+            }
+        }, (err, res, body) => {
+            if(err) throw new Error('Oops. this should not have happend', err);
+            if(body.comment == undefined) throw new Error('Something failed while Creating your Post. API Response:', body);
+            else completed = true;
+        });
+    }
+
     return completed;
 };
