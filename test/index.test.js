5c38158f0d4e9afd2acba82cba063d435b0191b5
Felix Fong <moongod101@hotmail.com>
2018-05-13 00:52:52 +0800

    Fixed the incorrect testing URL
    http -> https

--- test/index.test.js
+++ test/index.test.js
@@ -14,40 +14,40 @@ const {
 describe('how do the enpoints react..', () => {
 
     it('when a login api call is recived', () => {
-        expect(login).toBe('http://service.narvii.com/api/v1/g/s/auth/login')
+        expect(login).toBe('https://service.narvii.com/api/v1/g/s/auth/login')
     });
 
     it('when a Call for loading the user has arrvied', () => {
-        expect(getMe).toBe('http://service.narvii.com/api/v1/g/s/account');
+        expect(getMe).toBe('https://service.narvii.com/api/v1/g/s/account');
     });
 
     it('when a community load api call is recived', () => {
-        expect(getComs).toBe('http://service.narvii.com/api/v1/g/s/community/joined?start=0&size=50');
+        expect(getComs).toBe('https://service.narvii.com/api/v1/g/s/community/joined?start=0&size=50');
     });
 
     it('when a upload api call is recived', () => {
-        expect(upload).toBe('http://service.narvii.com/api/v1/g/s/media/upload');
+        expect(upload).toBe('https://service.narvii.com/api/v1/g/s/media/upload');
     })
 
     it('when a chat thread load api call is recived', () => {
         expect(
             getJoinedChats('xtesting')
         )
-        .toBe('http://service.narvii.com/api/v1/xtesting/s/chat/thread?type=joined-me&start=0&size=100');
+        .toBe('https://service.narvii.com/api/v1/xtesting/s/chat/thread?type=joined-me&start=0&size=100');
     });
 
     it('when a chat api call is recived', () => {
         expect(
             loadChat('xtesting', 'a_great_uuid', 7)
         )
-        .toBe('http://service.narvii.com/api/v1/xtesting/s/chat/thread/a_great_uuid/message?start=0&size=7&cv=v1.2');
+        .toBe('https://service.narvii.com/api/v1/xtesting/s/chat/thread/a_great_uuid/message?start=0&size=7&cv=v1.2');
     });
 
     it('when a sending a chat message api call is recived', () => {
         expect(
             sendChat('xtesting', 'a_great_uuid')
         )
-        .toBe('http://service.narvii.com/api/v1/xtesting/s/chat/thread/a_great_uuid/message');
+        .toBe('https://service.narvii.com/api/v1/xtesting/s/chat/thread/a_great_uuid/message');
     });
 
 });
