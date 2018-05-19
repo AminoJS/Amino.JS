8b8636281965cc9250d2b5fb9b98ea89dc9a8bb2
Felix Fong <moongod101@hotmail.com>
2018-05-20 01:23:02 +0800

    Wrap the variables into one single endpoints object

--- test/index.test.js
+++ test/index.test.js
@@ -1,59 +1,50 @@
 const index = require('../index');
 const sorter = require('../helpers/sorter');
-const {
-    login,
-    getMe,
-    getComs,
-    upload,
-    getJoinedChats,
-	getUserBlogs,
-    loadChat,
-    sendChat
-} = require('../helpers/endpoints');
+const endpoints = require('../helpers/endpoints');
 
 // Making sure that no one will accidentally missed the endpoints
 describe('how do the enpoints react..', () => {
 
     it('when a login api call is recived', () => {
-        expect(login).toBe('https://service.narvii.com/api/v1/g/s/auth/login')
+        expect(endpoints.login).toBe('https://service.narvii.com/api/v1/g/s/auth/login')
     });
 
     it('when a Call for loading the user has arrvied', () => {
-        expect(getMe).toBe('https://service.narvii.com/api/v1/g/s/account');
+        expect(endpoints.getMe).toBe('https://service.narvii.com/api/v1/g/s/account');
     });
 
     it('when a community load api call is recived', () => {
-        expect(getComs).toBe('https://service.narvii.com/api/v1/g/s/community/joined?start=0&size=50');
+        expect(endpoints.getComs).toBe('https://service.narvii.com/api/v1/g/s/community/joined?start=0&size=50');
     });
 
     it('when a upload api call is recived', () => {
-        expect(upload).toBe('https://service.narvii.com/api/v1/g/s/media/upload');
+        expect(endpoints.upload).toBe('https://service.narvii.com/api/v1/g/s/media/upload');
     })
 
     it('when a chat thread load api call is recived', () => {
         expect(
-            getJoinedChats('xtesting')
+            endpoints.getJoinedChats('xtesting')
         )
         .toBe('https://service.narvii.com/api/v1/xtesting/s/chat/thread?type=joined-me&start=0&size=100');
     });
 	
 	it('when a user blog thread load api call is recived', () => {
         expect(
-            getUserBlogs('xtesting', 'utesting', '100')
+            endpoints.getUserBlogs('xtesting', 'utesting', '100')
         )
         .toBe('https://service.narvii.com/api/v1/xtesting/s/blog?type=user&q=utesting&start=0&size=100');
     });
 
     it('when a chat api call is recived', () => {
         expect(
-            loadChat('xtesting', 'a_great_uuid', 7)
+            endpoints.loadChat('xtesting', 'a_great_uuid', 7)
         )
         .toBe('https://service.narvii.com/api/v1/xtesting/s/chat/thread/a_great_uuid/message?start=0&size=7&cv=v1.2');
     });
 
     it('when a sending a chat message api call is recived', () => {
         expect(
-            sendChat('xtesting', 'a_great_uuid')
+            endpoints.sendChat('xtesting', 'a_great_uuid')
         )
         .toBe('https://service.narvii.com/api/v1/xtesting/s/chat/thread/a_great_uuid/message');
     });
