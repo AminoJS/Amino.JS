eac3bd6362194bef964bfccd7ba07be80718f71e
Felix Fong <moongod101@hotmail.com>
2018-05-30 11:34:55 +0800

    Updated the test case to cover more area

--- test/modules/getJoinedComs.test.js
+++ test/modules/getJoinedComs.test.js
@@ -4,9 +4,13 @@ if(process.env.TRAVIS_PULL_REQUEST === 'false' || typeof process.env.TRAVIS_PULL
         it('should return the user\'s personal information like UID, username and icon', async () => {
             const Amino = require('../../index');
             const sid = await Amino.login(process.env.AMINO_EMAIL, process.env.AMINO_PASSWORD);
-            const getMyProfile = await Amino.getJoinedComs();
-            expect(getMyProfile.status).toBe('ok');
-            expect(getMyProfile.error).toBeNull();
+            const getJoinedComs = await Amino.getJoinedComs();
+            expect(getJoinedComs.coms).toBeDefined();
+            expect(
+                Array.isArray(getJoinedComs.coms)
+            ).toBe(true);
+            expect(getJoinedComs.status).toBe('ok');
+            expect(getJoinedComs.error).toBeNull();
         });
     });
 }else{
