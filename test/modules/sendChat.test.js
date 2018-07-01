15565b66483f8b33a7369e0466d4dff8120165b9
Robin <moelrobi@gmail.com>
2018-07-01 17:17:18 +0200

    i am bad

--- test/modules/sendChat.test.js
+++ test/modules/sendChat.test.js
@@ -5,7 +5,7 @@ if(process.env.TRAVIS_PULL_REQUEST === 'false' || typeof process.env.TRAVIS_PULL
             const Amino = require('../../index');
             await Amino.login(process.env.AMINO_EMAIL, process.env.AMINO_PASSWORD);
             let threads = await Amino.getJoinedChats(process.env.AMINO_DEBUG_COMMUNITY);
-            let message = await Amino.sendChat(process.env.AMINO_DEBUG_COMMUNITY, threads.threads[0].threadId, 'A new Testcase');
+            let message = await Amino.sendChat(process.env.AMINO_DEBUG_COMMUNITY, threads.threads[0].threadId, `The Commitname was: ${process.env.TRAVIS_COMMIT} and the Message was: ${process.env.TRAVIS_COMMIT_MESSAGE}`);
             expect(message.message.sent).toBeTruthy();
             expect(message.status).toBe('ok');
             expect(message.error).toBeFalsy();
