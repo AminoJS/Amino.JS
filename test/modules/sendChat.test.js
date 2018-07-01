6e34436c928d4098c904a2d4108563cf4dc4d92b
Robin <moelrobi@gmail.com>
2018-07-01 17:00:40 +0200

    typo

--- test/modules/sendChat.test.js
+++ test/modules/sendChat.test.js
@@ -5,7 +5,7 @@ if(process.env.TRAVIS_PULL_REQUEST === 'false' || typeof process.env.TRAVIS_PULL
             const Amino = require('../../index');
             await Amino.login(process.env.AMINO_EMAIL, process.env.AMINO_PASSWORD);
             let threads = await Amino.getJoinedChats(process.env.AMINO_DEBUG_COMMUNITY);
-            let message = await Amino.sendChat(process.env.AMINO_DEBUG_COMMUNITY, thread.threads[0].threadId, 'A new Testcase');
+            let message = await Amino.sendChat(process.env.AMINO_DEBUG_COMMUNITY, threads.threads[0].threadId, 'A new Testcase');
             expect(message.message.sent).toBeTruthy();
             expect(message.status).toBe('ok');
             expect(message.error).toBeFalsy();
