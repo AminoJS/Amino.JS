bddfa84a38f5855fe41baf8b75e42e18ab9aa5f2
Felix Fong <moongod101@hotmail.com>
2018-11-04 23:24:26 +0800

    Create a new unit test case for the getAdminLogs module

---
+++ test/modules/getAdminLog.js
@@ -0,0 +1,21 @@
+if(process.env.TRAVIS_PULL_REQUEST === 'false' || typeof process.env.TRAVIS_PULL_REQUEST === 'undefined'){
+    require('../helpers/loadEnv');
+    describe('how the methods should response (With NO false input)', () => {
+        it('Get a list of logs from one of the community', async () => {
+            const Amino = require('../../index');
+            await Amino.login(process.env.AMINO_EMAIL, process.env.AMINO_PASSWORD);
+            const myCommunities = await Amino.getJoinedComs();
+            const firstCommunity = myCommunities.coms[0];
+            const logs = await Amino.getAdminLogs(firstCommunity.id);
+            expect(logs.logs).toBeDefined();
+            expect(
+                Array.isArray(logs.logs)
+            )
+            .toBe(true);
+            expect(myChatRooms.status).toBe('ok');
+            expect(myChatRooms.error).toBeNull();
+        });
+    });
+}else{
+    describe.skip();
+}
